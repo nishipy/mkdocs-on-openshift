a6f135f04518117be7a0a52dd6c8f610ae077a2e
nishipy <goodisonev4@gmail.com>
2020-11-03 01:47:19 +0900

    Update test code (Java)

--- test/java-selenium/src/main/java/test/selenium/Main.java
+++ test/java-selenium/src/main/java/test/selenium/Main.java
@@ -4,9 +4,10 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.*;
 
 public class Main {
 	public static void main(String args[]) throws Exception {
@@ -21,7 +22,15 @@ public class Main {
 
 	@Test
 	public void checkTitle(String url) throws Exception{
-		WebDriver driver=new ChromeDriver();
+        
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        ChromeDriver driver = new ChromeDriver(options);
+        driver.manage().window().setSize(new Dimension(1200, 1000));
+
 		driver.get(url);
 		try {
 			WebElement elements = driver.findElement(By.name("query"));
