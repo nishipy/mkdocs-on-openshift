2c750171cd0f37fb77be003fd62f53b9d34ffba6
nishipy <goodisonev4@gmail.com>
2020-11-04 17:05:28 +0900

    Update

---
+++ test/java-selenium/src/test/java/test/selenium/MainTest.java
@@ -0,0 +1,43 @@
+package test.selenium;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.*;
+
+public class MainTest {
+	@Test
+	public void checkTitle() throws Exception{
+		String url;
+		url = "http://localhost:8000/";
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        ChromeDriver driver = new ChromeDriver(options);
+        driver.manage().window().setSize(new Dimension(1200, 1000));
+
+		driver.get(url);
+		try {
+			WebElement elements = driver.findElement(By.name("query"));
+			elements.sendKeys("nishipy");
+			Thread.sleep(3000);
+
+			WebElement content = driver.findElement(By.className("md-search-result__link"));
+			content.click();
+			Thread.sleep(3000);
+
+			assertEquals("About - Test Docs", driver.getTitle());
+        } catch (Exception e) {
+            fail(e.getMessage());
+        } finally {
+			driver.close();
+		}
+	}
+}
